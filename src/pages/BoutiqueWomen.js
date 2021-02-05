5771516da16f882a6505454fcde4eb6f59b38988
Ibrahim <adeniyiib360@gmail.com>
2021-02-05 22:43:27 +0100

    minor fixes

--- src/pages/BoutiqueWomen.js
+++ src/pages/BoutiqueWomen.js
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
-import Img06 from '../assets/images/img06.jpg';
-import Img07 from '../assets/images/img07.jpg';
-import Img08 from '../assets/images/img08.jpg';
-import Img09 from '../assets/images/img09.jpg';
-import Img10 from '../assets/images/img10.jpg';
-import Img11 from '../assets/images/img11.jpg';
-import Img12 from '../assets/images/img12.jpg';
-import Img13 from '../assets/images/img13.jpg';
-import Img14 from '../assets/images/img14.jpg';
+import Img06 from '../assets/images/img06.jpg'
+import Img07 from '../assets/images/img07.jpg'
+import Img08 from '../assets/images/img08.jpg'
+import Img09 from '../assets/images/img09.jpg'
+import Img10 from '../assets/images/img10.jpg'
+import Img11 from '../assets/images/img11.jpg'
+import Img12 from '../assets/images/img12.jpg'
+import Img13 from '../assets/images/img13.jpg'
+import Img14 from '../assets/images/img14.jpg'
 
 const BoutiqueWomenProduct = ({ id, data }) => {
-  const [currentItem, setCurrentItem] = useState(null);
-  const switchDisplay = id === currentItem;
+  const [currentItem, setCurrentItem] = useState(null)
+  const switchDisplay = id === currentItem
 
   return (
     <div
@@ -33,9 +33,11 @@ const BoutiqueWomenProduct = ({ id, data }) => {
           <p className='boutique-women--product__description'>
             — {data.description}
           </p>
-          <p className='boutique-women--product__price'>
-            {switchDisplay ? `€${data.price}.00` : 'Add to shopping bag'}
-          </p>
+          {switchDisplay ? (
+            <p className='boutique-women--product__cta'>Add to shopping bag</p>
+          ) : (
+            <p className='boutique-women--product__price'>€{data.price}.00</p>
+          )}
         </div>
         <svg
           xmlns='http://www.w3.org/2000/svg'
@@ -53,20 +55,19 @@ const BoutiqueWomenProduct = ({ id, data }) => {
         </svg>
       </div>
     </div>
-  );
-};
+  )
+}
 
-function BoutiqueWomen({
-  boutiqueWomenProductData = defaultBoutiqueWomenProductData,
+function BoutiqueWomen ({
+  boutiqueWomenProductData = defaultBoutiqueWomenProductData
 }) {
   const [productArr, setProductArr] = useState([])
   useEffect(() => {
     const newArr = () => {
       const someArr = []
-      for (let i = 0; boutiqueWomenProductData.length > 0; ) {
+      for (let i = 0; boutiqueWomenProductData.length > 0;) {
         const alertNow = boutiqueWomenProductData.splice(0, 3)
         someArr.push(alertNow)
-        console.log(alertNow)
         setProductArr(someArr)
       }
     }
@@ -77,23 +78,23 @@ function BoutiqueWomen({
       {productArr.map((product, idx) => {
         return (
           <div key={idx} className='boutique-women'>
-          <div className='top-left'>
-            <BoutiqueWomenProduct id={product[0].id} data={product[0]} />
+            <div className='top-left'>
+              <BoutiqueWomenProduct id={product[0].id} data={product[0]} />
+            </div>
+            <div className='top-right'>
+              <BoutiqueWomenProduct id={product[1].id} data={product[1]} />
+            </div>
+            <div className='bottom'>
+              <BoutiqueWomenProduct id={product[2].id} data={product[2]} />
+            </div>
           </div>
-          <div className='top-right'>
-            <BoutiqueWomenProduct id={product[1].id} data={product[1]} />
-          </div>
-          <div className='bottom'>
-            <BoutiqueWomenProduct id={product[2].id} data={product[2]} />
-          </div>
-        </div>
-        );
+        )
       })}
     </>
-  );
+  )
 }
 
-export default BoutiqueWomen;
+export default BoutiqueWomen
 
 const defaultBoutiqueWomenProductData = [
   {
@@ -101,62 +102,62 @@ const defaultBoutiqueWomenProductData = [
     imageOne: Img06,
     imageTwo: Img07,
     description: ' THE MOST LUXURIOUS T-SHIRT IN THE WORLD',
-    price: '500',
+    price: '500'
   },
   {
     id: 1,
     imageOne: Img07,
     imageTwo: Img08,
     description: ' THE MOST LUXURIOUS T-SHIRT IN THE WORLD',
-    price: '500',
+    price: '500'
   },
   {
     id: 2,
     imageOne: Img08,
     imageTwo: Img09,
     description: ' THE MOST LUXURIOUS T-SHIRT IN THE WORLD',
-    price: '500',
+    price: '500'
   },
   {
     id: 3,
     imageOne: Img09,
     imageTwo: Img10,
     description: ' THE MOST LUXURIOUS T-SHIRT IN THE WORLD',
-    price: '500',
+    price: '500'
   },
   {
     id: 4,
     imageOne: Img10,
     imageTwo: Img11,
     description: ' THE MOST LUXURIOUS T-SHIRT IN THE WORLD',
-    price: '500',
+    price: '500'
   },
   {
     id: 5,
     imageOne: Img11,
     imageTwo: Img12,
     description: ' THE MOST LUXURIOUS T-SHIRT IN THE WORLD',
-    price: '500',
+    price: '500'
   },
   {
     id: 6,
     imageOne: Img12,
     imageTwo: Img13,
     description: ' THE MOST LUXURIOUS T-SHIRT IN THE WORLD',
-    price: '500',
+    price: '500'
   },
   {
     id: 7,
     imageOne: Img13,
     imageTwo: Img14,
     description: ' THE MOST LUXURIOUS T-SHIRT IN THE WORLD',
-    price: '500',
+    price: '500'
   },
   {
     id: 8,
     imageOne: Img14,
     imageTwo: Img06,
     description: ' THE MOST LUXURIOUS T-SHIRT IN THE WORLD',
-    price: '500',
-  },
-];
+    price: '500'
+  }
+]
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import Header from '../components/Header'
-import Footer from '../components/Footer'
 import Product from '../components/Product'
 import Gallery from '../components/Gallery'
 
